ec6d6c056fb3606bc58572a39424c14cea302800
S. Park <tutumd96@naver.com>
2020-03-05 11:29:09 +0900

    파이차트

--- piechart.py
+++ piechart.py
@@ -1,7 +1,5 @@
 f, ax = plt.subplots(2,4, figsize=(20,10))
 
-#
-
 ax[0,2] = patient.region.value_counts().plot(kind='pie', labels = patient.region.unique(), title='nums of infected by region', ax=ax[0,2], autopct='%.2f%%')
 ax[0,2].legend(loc = 'best', labels=patient.region.unique()[:5])
 
@@ -9,5 +7,3 @@ ax[0,3] = patient.group.value_counts().plot(kind='pie', labels = patient.group.u
 
 ax[1,0] = patient.infection_reason.value_counts().plot(kind='pie', labels = patient.infection_reason.value_counts(), title='infection_reason', ax=ax[1,0], autopct='%.2f%%')
 ax[1,0].legend(loc='best', labels=patient.infection_reason.unique()[:3])
-
-#
