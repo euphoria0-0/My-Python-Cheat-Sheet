f02ecbb3adfc355b2d32012ab5641faaf4265c59
S. Park <tutumd96@naver.com>
2020-03-09 10:34:12 +0900

    모델 저장하기

---
+++ save_model.py
@@ -0,0 +1,10 @@
+# save model
+def save_model(model):
+	model_json = model.to_json()
+	with open(model_path+model_name+'.json', 'w') as f: 
+    		f.write(model_json)
+	model.save_weights(model_path+model_name+'.h5')
+
+# save tokenizer(tokenizer):
+	with open(path+'tokenizer.pickle', 'wb') as f:
+    		pickle.dump(tokenizer, f, protocol = pickle.HIGHEST_PROTOCOL)
