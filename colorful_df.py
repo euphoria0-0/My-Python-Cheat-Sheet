aa31566e33ec824f79144750aba71aa6d39e56e7
S. Park <tutumd96@naver.com>
2020-04-23 14:31:36 +0900

    Update colorful_df.py

--- colorful_df.py
+++ colorful_df.py
@@ -1,2 +1,2 @@
 cm = sns.light_palette("skyblue", as_cmap=True)
-train.describe().style.set_caption('Colormaps, with a caption.').background_gradient(cmap=cm)
+train.describe().style.background_gradient(cmap=cm)
