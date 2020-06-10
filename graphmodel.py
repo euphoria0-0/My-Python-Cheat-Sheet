0e92f4a901e117cf5c9485e28d6023550368501c
S. Park <tutumd96@naver.com>
2020-06-10 17:51:24 +0900

    Update graphmodel.py

--- graphmodel.py
+++ graphmodel.py
@@ -1,10 +1,9 @@
 N = 3000
 w = 2
 
-data = pd.read_csv(f)
-
-date = str(date).zfill(2)
-df = df.sort_values(["value"], ascending=[False])[:N]
+node = pd.read_csv(f2) # 지역 코드와 x,y좌표, 지역 
+data = pd.read_csv(f1) # 출발, 도착, value
+data = data.sort_values(["value"], ascending=[False])[:N]
 
 # 베이스 지도
 m = folium.Map(
@@ -13,22 +12,22 @@ zoom_start=11.5
 )
 
 # 매핑
-for ix, row in df.iterrows():
-	start = tuple(nodes[nodes['code'] == row['출발지']][['y','x']].iloc[0])
-	end = tuple(nodes[nodes['code'] == row['도착지']][['y','x']].iloc[0])
+for i, r in df.iterrows():
+	start = tuple(node[node['code'] == r['출발지']][['y','x']].iloc[0])
+	end = tuple(node[node['code'] == r['도착지']][['y','x']].iloc[0])
 	
 	# 포인트
 	for p in [start, end]:
 		folium.Circle(
 			location=p, radius=30, color='white', weight=0.5, 
 			fill_opacity=0.4, fill_color='red', fill=True,
-			popup=str(nodes[nodes['code'] == row['출발지']]['지역이름'])
+			popup=str(node[node['code'] == r['출발지']]['지역이름']) # 인코딩 확인 필요
 		).add_to(m)
 	
 	# 경로
 	folium.PolyLine(
 		locations=[start, end],
-		weight = row['value']/100*w,
+		weight = r['value']/100*w,
 		color='blue',
 		opacity=0.5,
 	).add_to(m)
